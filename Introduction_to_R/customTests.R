f33f6505c764449d0aff02869298893d9098f4a0
Kyle Coombs <kyle.g.coombs@gmail.com>
2024-09-01 16:05:33 -0400

    updated to output log

--- Introduction_to_R/customTests.R
+++ Introduction_to_R/customTests.R
@@ -10,3 +10,20 @@ check_equality_of_data = function(correctVal) {
   e = get('e', parent.frame())
   return(identical(correctVal, e$val))
 }
+
+getState <- function(){
+  # Whenever swirl is running, its callback is at the top of its call stack.
+  # Swirl's state, named e, is stored in the environment of the callback.
+  environment(sys.function(1))$e
+}
+
+# Retrieve the log from swirl's state
+getLog <- function(){
+  return(getState()$log)
+}
+
+# Save the log to an .Rdata file
+saveLog <- function(){
+  log <- getLog()
+  save(log, file = "log.Rdata")
+}
