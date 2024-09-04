e56a96cc5c8cbb6209548bdb92bbbacb22595d0a
Kyle Coombs <kyle.g.coombs@gmail.com>
2024-09-03 20:25:34 -0400

    adding in common functions like google forms

--- GGPlot_Demo/customTests.R
+++ GGPlot_Demo/customTests.R
@@ -11,19 +11,4 @@ check_equality_of_data = function(correctVal) {
   return(identical(correctVal, e$val))
 }
 
-getState <- function(){
-  # Whenever swirl is running, its callback is at the top of its call stack.
-  # Swirl's state, named e, is stored in the environment of the callback.
-  environment(sys.function(1))$e
-}
-
-# Retrieve the log from swirl's state
-getLog <- function(){
-  return(getState()$log)
-}
-
-# Save the log to an .Rdata file
-saveLog <- function(){
-  log <- getLog()
-  save(log, file = "log.Rdata")
-}
+source('../common_functions.R')
--- Introduction_to_R/customTests.R
+++ Introduction_to_R/customTests.R
@@ -1,3 +1,5 @@
+source('../common_functions.R')
+
 check_equality_of_vector = function(correctVal) {
   e = get('e', parent.frame())
   if (length(correctVal) != length(e$val)) {
@@ -9,21 +11,4 @@ check_equality_of_vector = function(correctVal) {
 check_equality_of_data = function(correctVal) {
   e = get('e', parent.frame())
   return(identical(correctVal, e$val))
-}
-
-getState <- function(){
-  # Whenever swirl is running, its callback is at the top of its call stack.
-  # Swirl's state, named e, is stored in the environment of the callback.
-  environment(sys.function(1))$e
-}
-
-# Retrieve the log from swirl's state
-getLog <- function(){
-  return(getState()$log)
-}
-
-# Save the log to an .Rdata file
-saveLog <- function(){
-  log <- getLog()
-  save(log, file = "log.Rdata")
 }
