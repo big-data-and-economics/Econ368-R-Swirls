ccb19faf2c94fbdb7a47ed70227de7a9c38c5a7f
Kyle Coombs <kyle.g.coombs@gmail.com>
2024-09-05 08:17:21 -0400

    custom naming

--- GGPlot_Demo/customTests.R
+++ GGPlot_Demo/customTests.R
@@ -40,7 +40,7 @@ submit_log <- function(){
   }
   
   p <- function(x, p, f, l = length(x)){if(l < p){x <- c(x, rep(f, p - l))};x}
-  
+
   log_ <- getLog()
   nrow_ <- max(unlist(lapply(log_, length)))
   log_tbl <- data.frame(user = rep(log_$user, nrow_),
@@ -53,8 +53,9 @@ submit_log <- function(){
                         encoded_skip = ifelse(is.na(log_$skipped),NA,base64encode(log_$skipped)),
                         datetime = p(log_$datetime, nrow_, NA),
                         stringsAsFactors = FALSE)
-  write.csv(log_tbl, file = 'log.csv', row.names = FALSE)
-  encoded_log <- base64encode('log.csv')
+  filename = paste("swirl_log", log_tbl$user[1], log_tbl$course_name[1], log_tbl$lesson_name[1], ".csv", sep = "_")
+  write.csv(log_tbl, file = filename, row.names = FALSE)
+  encoded_log <- base64encode(filename)
   browseURL(paste0(pre_fill_link, encoded_log))
   # output_hash='InVzZXIiLCJjb3Vyc2VfbmFtZSIsImxlc3Nvbl9uYW1lIiwicXVlc3Rpb25fbnVtYmVyIiwiY29ycmVjdCIsImF0dGVtcHQiLCJza2lwcGVkIiwiZW5jb2RlZF9za2lwIiwiZGF0ZXRpbWUiDQoiYXV0aG9yIiwiRGF0YSBTY2llbmNlIGZvciBFY29ub21pc3RzIiwiR0dQbG90MiBEZW1vIiwxNixUUlVFLDEsRkFMU0UsIkFBPT0iLDE3MjU1Mzg0NTQuOTYwNDgNCiJhdXRob3IiLCJEYXRhIFNjaWVuY2UgZm9yIEVjb25vbWlzdHMiLCJHR1Bsb3QyIERlbW8iLDE4LE5BLDEsTkEsIkFBPT0iLDE3MjU1Mzg0NTcuODU0MTgNCg=='
 }
